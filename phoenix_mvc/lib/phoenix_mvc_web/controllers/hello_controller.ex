6b75940ea7fe5ebe71d33e365f783532e0fd11c7
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-11-20 13:57:10 -0300

    Using URL params

--- phoenix_mvc/lib/phoenix_mvc_web/controllers/hello_controller.ex
+++ phoenix_mvc/lib/phoenix_mvc_web/controllers/hello_controller.ex
@@ -4,4 +4,8 @@ defmodule PhoenixMvcWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
 end
