1c6ec4e456e0b201ddef2181322659e0d10aa27c
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2021-01-26 22:14:16 -0300

    simple FizzBuzz

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,18 +1,34 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for FizzBuzz.
-  """
+  def is_fizz(number) do
+    if (rem(number, 3) === 0) do
+      true
+    else
+      false
+    end
+  end
 
-  @doc """
-  Hello world.
+  def is_buzz(number) do 
+    if (rem(number, 5) === 0) do
+      true
+    else
+      false
+    end
+  end
 
-  ## Examples
+  def is_fizz_buzz(number) do
+    if (is_fizz(number) and is_buzz(number)) do
+      true
+    else
+      false
+    end
+  end
 
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main do
+    Enum.each([1, 3, 9, 12], fn (v) ->
+      IO.puts "#{v}"
+      IO.puts "#{is_fizz(v)}"
+      IO.puts "#{is_buzz(v)}"
+      IO.puts "#{is_fizz_buzz(v)}"
+    end)
   end
 end
